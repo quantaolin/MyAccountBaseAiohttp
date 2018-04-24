ba892a8093796f8b12d1653aac8242784824da2e
linqt <linqt@windows10.microdone.cn>
2018-04-24 23:39:27 +0800

    1

---
+++ myaccount/countroller/openaccount.py
@@ -0,0 +1,12 @@
+from myaccount.routes import routes
+from aiohttp import web
+'''
+Created on 2018Äê4ÔÂ24ÈÕ
+
+@author: linqt
+'''
+
+@routes.post('/openacc')
+async def handler(request):
+    data = {'some': 'data'}
+    return web.json_response(data)
