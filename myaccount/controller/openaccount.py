a3f8ca9af956cd40995725618f704c8640ab3276
linqt <linqt@windows10.microdone.cn>
2018-04-27 00:17:02 +0800

    1

--- myaccount/controller/openaccount.py
+++ myaccount/controller/openaccount.py
@@ -10,7 +10,7 @@ Created on 2018-04-25 11:08:35
 @routes.get('/openacc')
 async def openacc(request):
     sqlstr="insert into bus.user (user_id,user_name,sex,card_num) values (%s,%s,%s,%s)"
-    param=('42aq','Tom','02','4444444')
+    param=('42aqe','Tom','02','4444444')
     r= await db.excute_insertorupdate( request.app['db'],sqlstr,param)
     print(r)
     data = {'result': r}
@@ -19,7 +19,7 @@ async def openacc(request):
 @routes.get('/queryacc')
 async def queryacc(request):
     sqlstr="select * from bus.user where user_id = %s"
-    param=('123456')
+    param=('42aqe')
     r= await db.excrte_select_dic(request.app['db'],sqlstr,param)
     print(r)
     data = {'result': r}
