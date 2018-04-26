92a19230372b760f7b1823ecb1c01bf3d7b22702
linqt <linqt@windows10.microdone.cn>
2018-04-26 23:39:25 +0800

    1

--- myaccount/controller/openaccount.py
+++ myaccount/controller/openaccount.py
@@ -7,5 +7,11 @@ Created on 2018-04-25 11:08:35
 
 @routes.get('/openacc')
 async def openacc(request):
-    data = {'some': 'data'}
+    sqlstr="insert into bus.user (user_id,user_name,sex,card_num) values ('123456','Jack','01','124124151233')"
+    async with request.app['db'].acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute(sqlstr)
+            r= cur.rowcount
+            print(r)
+    data = {'result': r}
     return data
