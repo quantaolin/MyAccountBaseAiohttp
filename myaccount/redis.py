b661db7145d625c9f97a672cddb4e50cdbbc1707
quantaolin <linqt2006@163.com>
2018-05-05 23:05:31 +0800

    1

---
+++ myaccount/redis.py
@@ -0,0 +1,23 @@
+import asyncio
+import aioredis
+'''
+Created on  2018-04-25 11:08:35
+
+@author: quantaolin
+'''
+async def redis_pool(app):
+    conf = app['config']['redis']
+    pool = await aioredis.create_pool(
+        (conf['host'],conf['port']),
+        password=conf['password'],
+        minsize=conf['minsize'],
+        maxsize=conf['maxsize'],
+        create_connection_timeout=None, 
+        loop=app.loop)
+    app['redis'] = pool
+    yield
+    app['redis'].close()
+    await app['redis'].wait_closed()
+    
+def setup_redis(app):
+    app.cleanup_ctx.append(redis_pool)
