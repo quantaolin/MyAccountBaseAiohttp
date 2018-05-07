767912e39ec76c0ca5cb8fd2b9db28345096fca1
quantaolin <quantaolin@bogon>
2018-05-07 14:30:57 +0800

    11

--- myaccount/main.py
+++ myaccount/main.py
@@ -51,7 +51,7 @@ def init(loop, argv):
 
 def main(argv):
     # init logging
-    fomstr='%(asctime)s -- %(process)d -- %(thread)d -- %(filename)s -- %(module)s -- %(funcName)s -- %(lineno)d : %(levelname)s -- %(message)s'
+    fomstr='%(asctime)s - %(process)d - %(thread)d - %(filename)s - %(module)s - %(funcName)s - %(lineno)d : %(levelname)s -- %(message)s'
     logging.basicConfig(
         level=logging.DEBUG,
         format=fomstr, 
